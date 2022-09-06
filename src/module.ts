4438244e152ba02ec9fea656698678d6a0314071
Sébastien Chopin <seb@nuxtjs.com>
2022-09-06 12:50:03 +0200

    chore: add types

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,16 @@ import { defineNuxtModule, createResolver, addComponent } from '@nuxt/kit'
 
 export interface ModuleOptions {}
 
+declare module '@nuxt/schema' {
+  interface AppConfig {
+    /** nuxt-icon configuration */
+    nuxtIcon: {
+      /** default size */
+      size: string
+    }
+  }
+}
+
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-icon',

a437b481865a015cb4e394fdbea159deee1222a8
Sébastien Chopin <seb@nuxtjs.com>
2022-09-08 13:13:59 +0200

    fix: errors on type-checking

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 import nuxtIcon from '../src/module'
 
 export default defineNuxtConfig({
+  typescript: { typeCheck: true, strict: true },
   modules: [
     nuxtIcon
   ]

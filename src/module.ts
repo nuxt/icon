c084305a5033b3d410b70db582b3b838159ada8c
Sébastien Chopin <seb@nuxt.com>
2023-01-13 15:11:41 +0100

    chore: add schema with nuxt-config-schema (#40)
    * chore: add schema with nuxt-config-schema
    
    * update config schema
    
    * enable pnpm in ci
    
    * use new schema:extend
    
    * style: format with prettier
    
    * chore: update metas
    
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,11 @@
-import { defineNuxtModule, createResolver, addComponent } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  createResolver,
+  addComponent,
+  installModule,
+} from '@nuxt/kit'
 
 export interface ModuleOptions {}
-
-declare module '@nuxt/schema' {
-  interface AppConfigInput {
-    /** nuxt-icon configuration */
-    nuxtIcon?: {
-      /** Default Icon size */
-      size?: string,
-      /** Default Icon class */
-      class?: string,
-      /** Icon name aliases */
-      aliases?: { [alias: string]: string }
-    }
-  }
-}
 
 // Learn how to create a Nuxt module on https://nuxt.com/docs/guide/going-further/modules/
 export default defineNuxtModule<ModuleOptions>({
@@ -22,17 +13,52 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-icon',
     configKey: 'icon',
     compatibility: {
-      nuxt: '^3.0.0-rc.9'
-    }
+      nuxt: '^3.0.0-rc.9',
+    },
   },
   defaults: {},
-  setup () {
+  setup(_options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
+
+    // Define types for the app.config compatible with Nuxt Studio
+    nuxt.hook('schema:extend', (schemas) => {
+      schemas.push({
+        nuxtIcon: {
+          $schema: {
+            description: 'Nuxt Icon',
+          },
+          size: {
+            $default: '',
+            $schema: {
+              description: 'Default size',
+              tags: ['@studio-icon material-symbols:format-size-rounded'],
+            },
+          },
+          class: {
+            $default: '',
+            $schema: {
+              description: 'Default class',
+              tags: ['@studio-icon material-symbols:css'],
+            },
+          },
+          aliases: {
+            $default: {},
+            $schema: {
+              description: 'Aliases',
+              tags: ['@studio-icon material-symbols:star-rounded'],
+              tsType: '{ [alias: string]: string }',
+            },
+          },
+        },
+      })
+    })
+
+    installModule('nuxt-config-schema')
 
     addComponent({
       name: 'Icon',
       global: true,
-      filePath: resolve('./runtime/Icon.vue')
+      filePath: resolve('./runtime/Icon.vue'),
     })
-  }
+  },
 })

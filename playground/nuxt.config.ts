3eca3f222f20a0e808fbbab2da1d474e300a29d7
Yaël Guilloux <yael.guilloux@gmail.com>
2023-01-25 14:50:42 +0100

    feat: autocomplete aliases (#45)
    * chore(playground): add typescript workspace to playground
    
    * feat(icon): add autocomplete on aliases on component

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 import nuxtIcon from '../src/module'
 
 export default defineNuxtConfig({
-  typescript: { strict: true },
+  typescript: { strict: true, includeWorkspace: true },
   modules: [
     nuxtIcon
   ]

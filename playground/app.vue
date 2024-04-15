9961f3ac31c785071f1eafe403280aa2d7ab5fa2
Daniel Roe <daniel@roe.dev>
2024-04-15 18:37:43 +0200

    chore: migrate to eslint v9 (#152)

--- playground/app.vue
+++ playground/app.vue
@@ -3,33 +3,73 @@
     <p>
       Sizes:
       <Icon name="uil:github" />
-      <Icon name="uil:github" size="24" />
-      <Icon name="uil:github" size="48" />
+      <Icon
+        name="uil:github"
+        size="24"
+      />
+      <Icon
+        name="uil:github"
+        size="48"
+      />
     </p>
     <p>
       Custom component:
       <Icon name="NuxtIcon" />
-      <Icon name="NuxtIcon" size="24" />
-      <Icon name="NuxtIcon" size="48" />
+      <Icon
+        name="NuxtIcon"
+        size="24"
+      />
+      <Icon
+        name="NuxtIcon"
+        size="48"
+      />
     </p>
     <p>
       Emoji:
       <Icon name="🚀" />
-      <Icon name="🚀" size="24" />
-      <Icon name="🚀" size="48" />
+      <Icon
+        name="🚀"
+        size="24"
+      />
+      <Icon
+        name="🚀"
+        size="48"
+      />
     </p>
     <p>
       Aliases:
-      <Icon name="github" size="24" />
-      <Icon name="nuxt" size="24" />
-      <Icon name="rocket" size="24" />
-      <Icon name="nxt" size="24" />
+      <Icon
+        name="github"
+        size="24"
+      />
+      <Icon
+        name="nuxt"
+        size="24"
+      />
+      <Icon
+        name="rocket"
+        size="24"
+      />
+      <Icon
+        name="nxt"
+        size="24"
+      />
     </p>
     <p>
       CSS:
-      <IconCSS name="uil:github" size="24" />
-      <IconCSS name="i-ph-rocket" size="48" />
-      <IconCSS name="nuxt" size="48" style="color: #00DC82;" />
+      <IconCSS
+        name="uil:github"
+        size="24"
+      />
+      <IconCSS
+        name="i-ph-rocket"
+        size="48"
+      />
+      <IconCSS
+        name="nuxt"
+        size="48"
+        style="color: #00DC82;"
+      />
     </p>
     <p>
       `i-` syntax:

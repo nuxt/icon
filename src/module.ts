15cef61e0d96f60feb7222b7f36c71bef264f58e
Daniel Roe <daniel@roe.dev>
2024-07-14 10:21:41 +0100

    chore: indicate compatibility with nuxt v4

--- src/module.ts
+++ src/module.ts
@@ -18,7 +18,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: '@nuxt/icon',
     configKey: 'icon',
     compatibility: {
-      nuxt: '^3.0.0',
+      nuxt: '>=3.0.0',
     },
   },
   defaults: {

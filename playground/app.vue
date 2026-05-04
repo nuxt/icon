6c3bc60c688923e3d0fb64cbbc5dd14109d79628
Sébastien Chopin <atinux@gmail.com>
2026-05-04 16:30:08 +0200

    chore: lint fix

--- playground/app.vue
+++ playground/app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import collections from '@iconify/collections/collections.json'
 import { useCookie } from '#imports'
 
